8ba7ee61839b1bdf015bc979dea65b3f0344ce77
Piknux11 <piknux11@gmail.com>
2025-05-29 23:39:26 +0000

    Programa de Peso ideal

---
+++ ProgramPronouns.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class ProgramPronouns {
+
+    public static void main(String[] args) {
+	
+	var userPronouns = switch(JOptionPane.showInputDialog("What's your pronouns?")) {
+	case "Men", "M", "m" -> "Men";
+	case "Woman", "W", "w" -> "Woman";
+	default -> "None";
+	};
+
+	var message = "You are a " + userPronouns + "\n" + "And your ideal weight is ";
+
+	var userHeight = Integer.parseInt(JOptionPane.showInputDialog("What's your height?"));
+
+	switch(userPronouns) {
+	case "Men":
+	    userHeight -= 110;
+	    break;
+	    
+	case "Woman":
+	    userHeight -= 120;
+	    break;
+	    
+	default:
+	    userHeight -= 110;
+	    break;
+	}
+
+	JOptionPane.showMessageDialog(null, message + userHeight);
+	
+    }
+}
