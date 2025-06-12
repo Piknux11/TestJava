70f529a4e8e74c1e339acad76c7db6103b6680d6
Piknux11 <piknux11@gmail.com>
2025-06-12 14:23:53 -0600

    Programa de matriz corregido

--- ProgramMatrix.java
+++ ProgramMatrix.java
@@ -1,43 +1,41 @@
+import java.util.Scanner;
 
 public class ProgramMatrix {
 
     public static void main(String[] args) {
-	float[][] interes = new float[3][6];
+        Scanner input = new Scanner(System.in);
 
-	FillArray1(interes[0]);
-	FillArray2(interes[1]);
+	//double salary = 10000.00;
+	double interest = 0.10;
 
-	Answer(interes);
-    }
-
-    public static void FillArray1(float[] array) {
-
-	for (int i=0 ; i<array.length ; i++) {
-	    array[i] = 10000.00f;
-	}
+	int numberOfYears = input.nextInt();
+	int numberOfInterest = input.nextInt();
 	
-    }
+	double[][] totals = new double[numberOfInterest][numberOfYears];
 
-    public static void FillArray2(float[] array) {
-	
-	for (int i=0 ; i<array.length ; i++) {
-	    array[i] = (10+i) * 0.01f;
-	}
-	
-    }
-
-    public static void Answer(float[][] array) {
-
-	for (int i=0 ; i<array[1].length ; i++) {
-	    array[2][i] = array[0][i] + (array[1][i] * array[0][i]); 
+	for (int i=0 ; i<totals.length ; i++) {
+	    totals[i][0] = 10000.00;
+	    
+	    for (int j=1 ; j<totals[1].length ; j++) {
+		totals[i][j] = 0.0;
+		totals[i][j] = totals[i][(j-1)] + (interest * totals[i][(j-1)]);
+		//salary = totals[i][j];
+	    }
+	    interest+=0.01;
+	    //salary = 10000.00;
 	}
 
-	for (float[] i : array) {
-	    for (float j : i) {
-		System.out.print(j + " ");
+	for (int i=0 ; i<totals[1].length ; i++) {
+	    //System.out.println("The interest is = " + (10 * (i+1)));
+	    for (int j=0 ; j<totals.length ; j++) {
+		//System.out.println("On the year " + (j+1));
+		System.out.print("[" + j + "][" + i + "] = ");
+		System.out.printf("%1.2f", totals[j][i]);
+		System.out.print(" ");
 	    }
 	    System.out.println();
 	}
+	
     }
     
 }
